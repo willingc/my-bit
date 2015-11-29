0067fd78e366371aa6ccdd5061e453927e9b85ad
Carol Willing <carolcode@willingconsulting.com>
2015-11-29 00:24:46 -0800

    Simplify program

--- happy.py
+++ happy.py
@@ -5,36 +5,33 @@ by Carol Willing
 November 28, 2015
 Public Domain
 
-Use this to display an image on micro:bit's 5x5 pixel grid of LEDs.
+Use this to display a 'Happy Face' image on micro:bit's 5x5 pixel grid of LEDs.
 
-We'll use the brightness setting of an LED to add creativity to image.
+Remember... Writing a program is similar to planning a birthday party.
 
-    Brightness:
-        9 for a bold outline (brightest)
-        6 for adding details (medium)
-        2 for shading        (dim light)
-        0 for off            (no light)
-
-Remember...
-Writing a program is similar to planning a birthday party.
-    Program     Birthday party
-    -------     --------------
-    'Prepare'   Prepare the room with balloons; order food; pick up a cake.
-    'Do'        Do things during the party -- sing, dance, play videogames.
-    'Clean'     Clean the table. Tidy up after the party. Take out the rubbish.
+Program       Birthday party
+-------       --------------
+'Prepare'     Prepare the room with balloons; order food; pick up a cake.
+'Do'          Do things during the party -- sing, dance, play videogames.
+'Clean'       Clean the table. Tidy up after the party. Take out the rubbish.
 
 """
 from microbit import *
 
-# Prepare stuff
+# Prepare. Put the preinstalled images into user friendly variables
 my_happy_face = Image.HAPPY
+my_sad_face = Image.SAD
 
-# Do stuff
+# Do things! ----> Show the images on the display.
 display.show(my_happy_face)
 sleep(8000)
 
-# Clean up stuff (yeah, you should clean your room too)
-display.clear()
+display.show(my_sad_face)
+sleep(8000)
+
+display.show(my_happy_face)
+sleep(4000)
+
+# Clean up stuff. Display 'BYE' and clear display. (Clean your room too.)
 display.scroll("BYE")
-sleep(1000)
 display.clear()
